5f9dd98f74079d16fbb40b3743247cfa3463ec41
Flavio Donzé <flavio.donze@scodi.ch>
2020-12-18 09:40:18 +0100

    fix warnings

--- src/main/java/org/keycloak/waffle/federation/NTLMFederationProvider.java
+++ src/main/java/org/keycloak/waffle/federation/NTLMFederationProvider.java
@@ -1,149 +1,145 @@
 package org.keycloak.waffle.federation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jboss.logging.Logger;
 import org.keycloak.credential.CredentialAuthentication;
 import org.keycloak.credential.CredentialInput;
-import org.keycloak.models.*;
+import org.keycloak.models.CredentialValidationOutput;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
+import org.keycloak.models.UserManager;
+import org.keycloak.models.UserModel;
 import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.UserStorageProviderModel;
 import org.keycloak.storage.user.ImportedUserValidation;
 import org.keycloak.waffle.NTLMCredentialInput;
+
 import waffle.windows.auth.IWindowsAccount;
 import waffle.windows.auth.IWindowsIdentity;
 
-import java.util.HashMap;
-import java.util.Map;
+public class NTLMFederationProvider implements UserStorageProvider, CredentialAuthentication, ImportedUserValidation {
+	private KeycloakSession session;
+	private UserStorageProviderModel userStorageProviderModel;
+	private static final Logger logger = Logger.getLogger(NTLMFederationProvider.class);
 
-public class NTLMFederationProvider implements UserStorageProvider,
-        CredentialAuthentication,
-        ImportedUserValidation {
-    private KeycloakSession session;
-    private UserStorageProviderModel userStorageProviderModel;
-    private NTLMFederationProviderFactory factory;
-    private static final Logger logger = Logger.getLogger(NTLMFederationProvider.class);
+	public NTLMFederationProvider(KeycloakSession session, UserStorageProviderModel userStorageProviderModel,
+			NTLMFederationProviderFactory ntlmFederationProviderFactory) {
+		this.session = session;
+		this.userStorageProviderModel = userStorageProviderModel;
+	}
 
-    public NTLMFederationProvider(KeycloakSession session
-            , UserStorageProviderModel userStorageProviderModel
-            , NTLMFederationProviderFactory ntlmFederationProviderFactory) {
-        this.session = session;
-        this.userStorageProviderModel = userStorageProviderModel;
-        factory = ntlmFederationProviderFactory;
-    }
+	@Override
+	public void close() {
+	}
 
-    @Override
-    public void close() {
-    }
+	@Override
+	public boolean supportsCredentialAuthenticationFor(String type) {
+		return NTLMCredentialInput.NTLM_CREDENTIAL_TYPE.equals(type);
+	}
 
-    @Override
-    public boolean supportsCredentialAuthenticationFor(String type) {
-        return NTLMCredentialInput.NTLM_CREDENTIAL_TYPE.equals(type);
-    }
+	@Override
+	public CredentialValidationOutput authenticate(RealmModel realm, CredentialInput input) {
+		if (!(input instanceof NTLMCredentialInput)) {
+			return null;
+		}
+		NTLMCredentialInput credential = (NTLMCredentialInput) input;
+		if (credential.getType().equals(NTLMCredentialInput.NTLM_CREDENTIAL_TYPE)) {
+			IWindowsIdentity windowsIdentity = credential.getWindowsIdentity();
 
-    @Override
-    public CredentialValidationOutput authenticate(RealmModel realm, CredentialInput input) {
-        if (!(input instanceof NTLMCredentialInput)) return null;
-        NTLMCredentialInput credential = (NTLMCredentialInput) input;
-        if (credential.getType().equals(NTLMCredentialInput.NTLM_CREDENTIAL_TYPE)) {
-            IWindowsIdentity windowsIdentity = credential.getWindowsIdentity();
+			Map<String, String> state = new HashMap<String, String>();
+			if (!windowsIdentity.isGuest()) {
+				String username = extractUserWithoutDomain(windowsIdentity.getFqn());
+				UserModel user = findOrCreateAuthenticatedUser(realm, username, windowsIdentity.getGroups(),
+						windowsIdentity.getSidString());
+				if (user == null) {
+					return CredentialValidationOutput.failed();
+				}
+				return new CredentialValidationOutput(user, CredentialValidationOutput.Status.AUTHENTICATED, state);
+			}
+		}
+		return null;
+	}
 
-            Map<String, String> state = new HashMap<String, String>();
-            if (!windowsIdentity.isGuest()) {
-                String username = extractUserWithoutDomain(windowsIdentity.getFqn());
-                UserModel user = findOrCreateAuthenticatedUser(realm
-                        , username
-                        , windowsIdentity.getGroups()
-                        , windowsIdentity.getSidString());
-                if (user == null) {
-                    return CredentialValidationOutput.failed();
-                } else {
-                    return new CredentialValidationOutput(user, CredentialValidationOutput.Status.AUTHENTICATED, state);
-                }
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
+	private UserModel findOrCreateAuthenticatedUser(RealmModel realm, String username, IWindowsAccount[] groups, String sidString) {
+		UserModel user = session.userLocalStorage().getUserByUsername(username, realm);
+		if (user != null) {
+			user = session.users().getUserById(user.getId(), realm); // make sure we get a cached instance
+			logger.debug("NTLM authenticated user " + username + " found in Keycloak storage");
 
-    private UserModel findOrCreateAuthenticatedUser(RealmModel realm, String username, IWindowsAccount[] groups, String sidString) {
-        UserModel user = session.userLocalStorage().getUserByUsername(username, realm);
-        if (user != null) {
-            user = session.users().getUserById(user.getId(), realm);  // make sure we get a cached instance
-            logger.debug("NTLM authenticated user " + username + " found in Keycloak storage");
+			if (!userStorageProviderModel.getId().equals(user.getFederationLink())) {
+				logger.warn("User with username " + username + " already exists, but is not linked to provider ["
+						+ userStorageProviderModel.getName() + "]");
+				return null;
+			}
 
-            if (!userStorageProviderModel.getId().equals(user.getFederationLink())) {
-                logger.warn("User with username " + username + " already exists, but is not linked to provider [" + userStorageProviderModel.getName() + "]");
-                return null;
-            } else {
-                onImportUserFromLDAP(groups, user, realm, false);
-                UserModel proxied = validate(realm, user);
-                if (proxied != null) {
-                    return proxied;
-                } else {
-                    logger.warn("User with username " + username + " already exists and is linked to provider [" + userStorageProviderModel.getName() +
-                            "] but NTLM principal is not correct. NTLM principal on user is: " + user.getId());
-                    logger.warn("Will re-create user");
-                    new UserManager(session).removeUser(realm, user, session.userLocalStorage());
-                }
-            }
-        }
+			onImportUserFromLDAP(groups, user, realm, false);
+			UserModel proxied = validate(realm, user);
+			if (proxied != null) {
+				return proxied;
+			}
 
-        logger.debug("Kerberos authenticated user " + username + " not in Keycloak storage. Creating him");
-        return importUserToKeycloak(realm, sidString, username, groups);
-    }
+			logger.warn(
+					"User with username " + username + " already exists and is linked to provider [" + userStorageProviderModel.getName()
+							+ "] but NTLM principal is not correct. NTLM principal on user is: " + user.getId());
+			logger.warn("Will re-create user");
+			new UserManager(session).removeUser(realm, user, session.userLocalStorage());
+		}
 
-    private UserModel importUserToKeycloak(RealmModel realm
-            , String sidString
-            , String username
-            , IWindowsAccount[] groups) {
-        // Just guessing email from kerberos realm
+		logger.debug("Kerberos authenticated user " + username + " not in Keycloak storage. Creating him");
+		return importUserToKeycloak(realm, sidString, username, groups);
+	}
 
-        logger.debugf("Creating NTLM user: %s, to local Keycloak storage", username);
-        UserModel user = session.userLocalStorage().addUser(realm, username);
-        user.setEnabled(true);
-        user.setFederationLink(userStorageProviderModel.getId());
-        user.setSingleAttribute("SID", sidString);
-        onImportUserFromLDAP(groups, user, realm, true);
+	private UserModel importUserToKeycloak(RealmModel realm, String sidString, String username, IWindowsAccount[] groups) {
+		// Just guessing email from kerberos realm
 
-        user.addRequiredAction(UserModel.RequiredAction.UPDATE_PROFILE);
+		logger.debugf("Creating NTLM user: %s, to local Keycloak storage", username);
+		UserModel user = session.userLocalStorage().addUser(realm, username);
+		user.setEnabled(true);
+		user.setFederationLink(userStorageProviderModel.getId());
+		user.setSingleAttribute("SID", sidString);
+		onImportUserFromLDAP(groups, user, realm, true);
 
-        return validate(realm, user);
-    }
+		user.addRequiredAction(UserModel.RequiredAction.UPDATE_PROFILE);
 
-    private String extractUserWithoutDomain(String username) {
-        if (username.contains("\\")) {
-            username = username.substring(username.indexOf("\\") + 1, username.length());
-        }
-        return username;
-    }
+		return validate(realm, user);
+	}
 
-    public boolean isValid(RealmModel realm, UserModel local) {
-        return true;
-    }
+	private String extractUserWithoutDomain(String username) {
+		if (username.contains("\\")) {
+			username = username.substring(username.indexOf("\\") + 1, username.length());
+		}
+		return username;
+	}
 
-    @Override
-    public UserModel validate(RealmModel realm, UserModel user) {
-        if (!isValid(realm, user)) {
-            return null;
-        }
+	public boolean isValid(RealmModel realm, UserModel local) {
+		return true;
+	}
 
-        return new ReadOnlyUserModelDelegate(user, this);
-    }
+	@Override
+	public UserModel validate(RealmModel realm, UserModel user) {
+		if (!isValid(realm, user)) {
+			return null;
+		}
 
-    public void onImportUserFromLDAP(IWindowsAccount[] groups, UserModel user, RealmModel realm, boolean isCreate) {
-        for (IWindowsAccount account : groups) {
-            String roleName = account.getName();
+		return new ReadOnlyUserModelDelegate(user, this);
+	}
 
-            RoleModel role = realm.getRole(roleName);
+	public void onImportUserFromLDAP(IWindowsAccount[] groups, UserModel user, RealmModel realm, boolean isCreate) {
+		for (IWindowsAccount account : groups) {
+			String roleName = account.getName();
 
-            if (role == null) {
-                role = realm.addRole(roleName);
-            }
+			RoleModel role = realm.getRole(roleName);
 
-            logger.debugf("Granting role [%s] to user [%s] during import from NTLM", roleName, user.getUsername());
-            user.grantRole(role);
-        }
-    }
+			if (role == null) {
+				role = realm.addRole(roleName);
+			}
+
+			logger.debugf("Granting role [%s] to user [%s] during import from NTLM", roleName, user.getUsername());
+			user.grantRole(role);
+		}
+	}
 
 }
